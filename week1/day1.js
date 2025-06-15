49e30e2902e7d048a0962684ec0d0b180e5253ad
joenwoke <jnwoke@hawk.iit.edu>
2025-06-14 22:31:29 -0700

    Added difference and quotient operators examples

--- week1/day1.js
+++ week1/day1.js
@@ -11,3 +11,10 @@ console.log(sum);
 // Checks if two operands are equal in both value and type, without performing type coercion and returning a boolean result
 let isEqual = ('5' === '5');
 console.log(isEqual);
+
+const difference = 100 - 2;
+console.log(difference);
+
+let quotient = 10/2;
+console.log(quotient);
+
