77f14e39c1b2c5ae60fd206c408d6592f2858413
joenwoke <jnwoke@hawk.iit.edu>
2025-06-15 22:47:00 -0700

    More control structures examples

--- week1/day2.js
+++ week1/day2.js
@@ -1,9 +1,12 @@
 //Control structures in JavaScript
 
 // if statement example
-let age = 17;
+let age = 9;
 if (age >= 18) {
     console.log("You are an adult"); }
+else if (age >=10) {
+    console.log("You are a teenager");
+}
 else {
     console.log("You are a child");
 }
