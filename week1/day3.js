013a085db9bac9c2ec920d313f75f0a0204c5cfa
joenwoke <jnwoke@hawk.iit.edu>
2025-06-16 22:03:21 -0700

    storing function as data and invoke

--- week1/day3.js
+++ week1/day3.js
@@ -3,3 +3,9 @@ function greet(name) {
     console.log("Hello, my name is " + name);
 }
 greet("Joseph");
+
+// Function expression
+const hello = function(name1) {
+    console.log("Hello,", name1);
+};
+hello("Daniel");
