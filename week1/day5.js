9308a1edd10bcc90b6760d0ef5168e48b419bc75
joenwoke <jnwoke@hawk.iit.edu>
2025-06-21 22:26:24 -0700

    Modifying array element Orange to Mango

--- week1/day5.js
+++ week1/day5.js
@@ -1,3 +1,7 @@
 // Arrays -> An array is like a shelf that holds multiple values in order
 let fruits = ["Apple", "Banana", "Orange"];
 console.log(fruits[0]);
+
+// To modify this array
+fruits[2] = "Mango";
+console.log(fruits);
