0d1cc6c8ff7089c2a3850bdef985578d828a9f04
joenwoke <jnwoke@hawk.iit.edu>
2025-06-22 23:28:43 -0700

    Further clarification on console.log code

--- week1/day5.js
+++ week1/day5.js
@@ -45,4 +45,5 @@ let nums = [1, 2, 3];
 
 for (let i = 0; i < nums.length; i++) {
     console.log(nums[i]);
+    // i = 0, meaning the nums[i], is same as nums[0], which is at index 0 in the array
 }
