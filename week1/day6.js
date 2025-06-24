1d53fb0901be0849709ae6b133e4988f0fbcb4bc
joenwoke <jnwoke@hawk.iit.edu>
2025-06-24 10:32:14 -0700

    Accessing values in the person object

--- week1/day6.js
+++ week1/day6.js
@@ -1,6 +1,11 @@
 // Objects: Key value data containers
+
+// Object example
 let person = {
     name: "Merlin",
     age: 22,
     isStudent: true
 };
+
+// To access values
+console.log(person.name);
