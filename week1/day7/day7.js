75d1a58dcf2d6e19c8061b9b66985b64c672fe23
joenwoke <jnwoke@hawk.iit.edu>
2025-07-07 18:27:24 -0700

    Using built in JS method to access HTML elements with specific id's

--- week1/day7/day7.js
+++ week1/day7/day7.js
@@ -1,1 +1,7 @@
 // Document Object Model - DOM Manipulation and events
+
+// Accessing elements in the DOM
+const title = document.getElementById("title");
+const button = document.getElementById("btn");
+
+console.log(title);
